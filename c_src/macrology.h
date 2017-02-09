aa75df48ca695f1c9e9ecc84cc213eb18f13a41e
Richard Kallos <rkallos@gmail.com>
2017-02-09 16:23:19 -0500

    Add lyrics to macrology.h

--- c_src/macrology.h
+++ c_src/macrology.h
@@ -1,3 +1,11 @@
+/* o/~ readings in macrology /
+ *     never meant that much to me /
+ *     the language I thought was just so /
+ *     she tells me, we ain't compatible
+ *
+ * Copyright 2015 AdGear Technologies Inc.
+ */
+
 #pragma once
 
 #define CONCAT_HELPER(x,y) x##y
